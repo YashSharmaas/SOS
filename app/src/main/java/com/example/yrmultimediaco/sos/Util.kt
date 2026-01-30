3b3d6679e06658c43a13e8087bb562e239c8fd10
YashSharmaas <97779523+YashSharmaas@users.noreply.github.com>
2026-01-30 10:36:37 +0530

    refactor Util.kt to encapsulate ttlForPriority in Util class

--- app/src/main/java/com/example/yrmultimediaco/sos/Util.kt
+++ app/src/main/java/com/example/yrmultimediaco/sos/Util.kt
@@ -1,9 +1,11 @@
 package com.example.yrmultimediaco.sos
 
-fun ttlForPriority(priority: Int): Long {
-    return when (priority) {
-        0 -> 10 * 60 * 1000L
-        1 -> 2 * 60 * 1000L
-        else -> 7 * 60 * 1000L
+class Util {
+    fun ttlForPriority(priority: Int): Long {
+        return when (priority) {
+            0 -> 10 * 60 * 1000L
+            1 -> 2 * 60 * 1000L
+            else -> 7 * 60 * 1000L
+        }
     }
 }
